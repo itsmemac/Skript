904cfa2b5bbdf6ea119af1e8d1a2283d0578fe65
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2026-01-01 00:22:44 +0300

    Optimize looping reversed lists (#8335)
    * Implement #iterator for ExprReversedList
    
    * Remove unnecessary array cloning
    
    * Implement KeyProviderExpression for ExprElement

--- src/main/java/ch/njol/skript/expressions/ExprReversedList.java
+++ src/main/java/ch/njol/skript/expressions/ExprReversedList.java
@@ -17,7 +17,10 @@ import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 @Name("Reversed List")
 @Description("Reverses given list.")
@@ -53,11 +56,27 @@ public class ExprReversedList extends SimpleExpression<Object> {
 	@Override
 	@Nullable
 	protected Object[] get(Event e) {
-		Object[] inputArray = list.getArray(e).clone();
-		Object[] array = (Object[]) Array.newInstance(getReturnType(), inputArray.length);
-		System.arraycopy(inputArray, 0, array, 0, inputArray.length);
+		Object[] array = list.getArray(e);
 		reverse(array);
 		return array;
+	}
+
+	@Override
+	public @Nullable Iterator<?> iterator(Event event) {
+		List<?> list = Arrays.asList(this.list.getArray(event));
+		return new Iterator<>() {
+			private final ListIterator<?> listIterator = list.listIterator(list.size());
+
+			@Override
+			public boolean hasNext() {
+				return listIterator.hasPrevious();
+			}
+
+			@Override
+			public Object next() {
+				return listIterator.previous();
+			}
+		};
 	}
 
 	@Override
