cffefae9ff3a82e25a335c8e4250bd7f40908152
Patrick Miller <apickledwalrus@icloud.com>
2026-01-01 18:05:16 -0500

    Resolve conflicts from merging dev/patch into dev/feature

--- src/main/java/ch/njol/skript/expressions/ExprYawPitch.java
+++ src/main/java/ch/njol/skript/expressions/ExprYawPitch.java
@@ -2,7 +2,7 @@ package ch.njol.skript.expressions;
 
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
-import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Example;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
