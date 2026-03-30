11d5b71ad4b555d2a28359c42a53f2ea0fea29fb
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2026-03-30 22:21:35 +0300

    Player Pick Item Event (#8331)

--- src/main/java/org/skriptlang/skript/bukkit/misc/MiscModule.java
+++ src/main/java/org/skriptlang/skript/bukkit/misc/MiscModule.java
@@ -1,9 +1,12 @@
 package org.skriptlang.skript.bukkit.misc;
 
+import ch.njol.skript.Skript;
 import org.skriptlang.skript.addon.AddonModule;
 import org.skriptlang.skript.addon.HierarchicalAddonModule;
 import org.skriptlang.skript.addon.SkriptAddon;
+import org.skriptlang.skript.bukkit.misc.elements.expressions.ExprPickedItem;
 import org.skriptlang.skript.bukkit.misc.elements.expressions.ExprWithYawPitch;
+import org.skriptlang.skript.bukkit.misc.events.EvtPlayerPickItem;
 
 public class MiscModule extends HierarchicalAddonModule {
 
@@ -16,6 +19,12 @@ public class MiscModule extends HierarchicalAddonModule {
 		register(addon,
 			ExprWithYawPitch::register
 		);
+		if (Skript.classExists("io.papermc.paper.event.player.PlayerPickBlockEvent")) {
+			register(addon,
+				EvtPlayerPickItem::register,
+				ExprPickedItem::register
+			);
+		}
 	}
 
 	@Override
